1feb7037dabc6d5d1eee99a1764bfe6635506e90
Williams25 <william.souza.timao@hotmail.com>
2022-08-16 06:55:30 -0300

    exportando novos componentes (Skills, AnimatedCircularProgress)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button";
 export { Header } from "./Header";
 export { Banner } from "./Banner";
 export { Container } from "./Container";
+export { Skills } from "./Skills";
+export { AnimatedCircularProgress } from "./AnimatedCircularProgress";
