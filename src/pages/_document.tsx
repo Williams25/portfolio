864b66ab64546ca432c5e92e251a14cb82899d55
Williams25 <william.souza.timao@hotmail.com>
2022-08-16 07:05:13 -0300

    adicionando meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,10 +41,7 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/img/icon-512.png" />
           <link rel="apple-touch-icon" href="/img/icon-512.png" />
           <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A simple project startter to work with TypeScript, React, NextJS and Styled Components"
-          />
+          <meta name="description" content="Portfolio - Williams25" />
         </Head>
         <body>
           <Main />
