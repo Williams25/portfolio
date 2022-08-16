7559683175676651a276d29022d4f00f4228509f
Williams25 <william.souza.timao@hotmail.com>
2022-08-16 09:03:50 -0300

    adicionando animações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from "next";
 import { Banner, Skills } from "src/components";
 import { skillsMock } from "src/components/Skills/mock";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+      mirror: true
+    });
+  }, []);
+
   return (
     <div>
       <Banner />
-
-      <Skills skills={skillsMock} />
+      <div data-aos="zoom-in-up">
+        <Skills skills={skillsMock} />
+      </div>
     </div>
   );
 };
