ec9a3cce21370ed2de8fdb3943d53abf428ab965
Williams25 <william.souza.timao@hotmail.com>
2022-09-09 05:50:11 -0300

    adicionado altura/largura minima para a logo

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -60,6 +60,8 @@ export const Header = styled.header<HeaderStyles>`
 
         & img {
           cursor: pointer;
+          min-width: 85px;
+          min-height: 25px;
         }
       }
       & .menu-items {
