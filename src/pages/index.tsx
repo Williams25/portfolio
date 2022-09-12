d72948ee197580b495d8340745e6a25ce8bb8fa5
Williams25 <william.souza.timao@hotmail.com>
2022-09-12 07:06:04 -0300

    recuperando skills  via ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,31 @@
-import type { NextPage } from "next";
+import type { GetStaticProps } from "next";
 import { Banner, Contact, Skills, Projects } from "src/components";
+import { SkillsProps } from "src/components/Skills";
 import { skillsMock } from "src/components/Skills/mock";
 
-const Home: NextPage = () => {
+type HomeProps = SkillsProps;
+
+const Home = ({ skills }: HomeProps) => {
   return (
     <main role="main">
       <Banner />
 
-      <Skills skills={skillsMock} />
+      <Skills skills={skills} />
 
       <Projects />
 
       <Contact />
     </main>
   );
+};
+
+export const getStaticProps: GetStaticProps<HomeProps> = () => {
+  return {
+    props: {
+      skills: skillsMock
+    },
+    revalidate: 60 * 60 * 1 // Cache response for 1 hour
+  };
 };
 
 export default Home;
