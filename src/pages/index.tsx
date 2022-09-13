62727c9c596d31e68e52ae0dd78ebe4351a48cfb
Williams25 <william.souza.timao@hotmail.com>
2022-09-13 09:16:39 -0300

    criando tipagem para renderizar os componentes com os dados via ssg e realizando chamada graphql para recuperar os dados da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,63 @@
 import type { GetStaticProps } from "next";
 import { Banner, Contact, Skills, Projects } from "src/components";
+import { BannerProps } from "src/components/Banner";
+import { ProjectsProps } from "src/components/Projects";
 import { SkillsProps } from "src/components/Skills";
-import { skillsMock } from "src/components/Skills/mock";
+import { client } from "src/graphql/apollo";
+import { getHome } from "src/graphql/queries/getHome";
+import { HomeApiResponse } from "src/types/HomeApiResponse";
 
-type HomeProps = SkillsProps;
+type HomeProps = {
+  banner: BannerProps;
+  projects: ProjectsProps;
+} & SkillsProps;
 
-const Home = ({ skills }: HomeProps) => {
+const Home = ({ skills, banner, projects }: HomeProps) => {
   return (
     <main role="main">
-      <Banner />
+      <Banner {...banner} />
 
       <Skills skills={skills} />
 
-      <Projects />
+      <Projects {...projects} />
 
       <Contact />
     </main>
   );
 };
 
-export const getStaticProps: GetStaticProps<HomeProps> = () => {
-  return {
-    props: {
-      skills: skillsMock
-    },
-    revalidate: 60 * 60 * 1 // Cache response for 1 hour
-  };
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  try {
+    const {
+      data: { home }
+    } = await client.query<HomeApiResponse>({
+      query: getHome,
+      variables: { id: process.env.NEXT_PUBLIC_HOME_ID }
+    });
+
+    return {
+      props: {
+        skills: home.skills,
+        banner: {
+          rotateText: home.rotateText,
+          title: home.title,
+          description: {
+            text: home.description.text
+          }
+        },
+        projects: {
+          projects: home.projects,
+          projectDescription: home.projectDescription,
+          title: home.projectTitle
+        }
+      },
+      revalidate: 60 * 60 * 1 // Cache response for 1 hour
+    };
+  } catch {
+    return {
+      notFound: true
+    };
+  }
 };
 
 export default Home;
