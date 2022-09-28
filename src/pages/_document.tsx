9e8449a04614c7e9480e075fe1d9ed2681acc550
Williams25 <william.souza.timao@hotmail.com>
2022-09-28 10:32:18 -0300

    adicionando metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,11 +39,15 @@ export default class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <meta charSet="UTF-8" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.williams25.com/" />
           <meta property="og:title" content="Portfolio - Williams25" />
           <meta property="og:description" content="Portfolio - Williams25" />
           <meta name="description" content="Portfolio - Williams25" />
           <meta property="og:image" content="/assets/img/logo.svg" />
+          <meta name="robots" content="index" />
 
+          <link rel="canonical" href="https://www.williams25.com/" />
           <link rel="icon" href="/assets/img/fav.svg" />
           <link rel="icon" href="/assets/img/fav.svg" />
           <link rel="apple-touch-icon" href="/assets/img/fav.png" />
