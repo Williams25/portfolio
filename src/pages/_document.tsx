1b7e1544bf060b3f19310daadd403a666924c2da
Williams25 <william.souza.timao@hotmail.com>
2022-09-28 10:46:58 -0300

    ajustando og:image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,10 @@ export default class MyDocument extends Document {
           <meta property="og:title" content="Portfolio - Williams25" />
           <meta property="og:description" content="Portfolio - Williams25" />
           <meta name="description" content="Portfolio - Williams25" />
-          <meta property="og:image" content="/assets/img/logo.svg" />
+          <meta
+            property="og:image"
+            content="https://www.williams25.com/assets/img/logo.svg"
+          />
           <meta name="robots" content="index" />
 
           <link rel="canonical" href="https://www.williams25.com/" />
